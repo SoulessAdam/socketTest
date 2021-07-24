0e58c75b9b327f1e47493c6ccf4851fe40f080a2
SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>
2021-07-24 21:18:11 +0100

    Server Message Handling Complete

--- server.py
+++ server.py
@@ -1,1 +1,32 @@
 import socket
+import threading
+
+
+HEADER = 64
+port = 6969
+server_ip = socket.gethostbyname(socket.gethostname())
+ADDR = (server_ip, port)
+DISCONNECT_MSG= "@disc"
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def client_handle(conn, addr):
+    print(f"[CONNECTION] {addr} connected.")
+    connected  = True
+    while connected:
+        msg_len = int(conn.recv(HEADER).decode("utf-8"))
+        msg = conn.recv(msg_len).decode("utf-8")
+        if msg == DISCONNECT_MSG: connected = False
+        print(f"[{ADDR} MSG] {msg}")
+
+    conn.close()
+
+def start():
+    server.listen()
+    while True:
+        conn, addr = server.accept()
+        threading.Thread(target=client_handle, args=(conn, addr)).start()
+        print(f"[CONNECTION] {threading.activeCount() -1}")
+
+print("Starting Server")
+start()
