de903cd7fc99b902b9cfb2c106457b8dd7ebe213
SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>
2022-03-12 12:31:13 +0000

    Removed unnecessary variables.

--- UDPServer.py
+++ UDPServer.py
@@ -1,15 +1,10 @@
 import socket
-import threading
-import os
-
 
 HEADER = 1024
 port = 6969
 FORMAT = "utf-8"
 server_ip = socket.gethostbyname(socket.gethostname())
 ADDR = (server_ip, port)
-DISCONNECT_MSG= "@disc"
-EXIT_MSG = "@killServer 1902-783298-238947"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind(ADDR)
